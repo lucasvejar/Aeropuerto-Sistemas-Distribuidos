ce5a010d893a1433464e587fe15cb17ad4fbe2ea
lucas <lucasficus@gmail.com>
2018-09-22 12:32:09 -0300

    añadi una ventana para mostrar el chat
    una ventana para el chat

---
+++ src/vista/ventanaChat.java
@@ -0,0 +1,43 @@
+package vista;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class ventanaChat {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ventanaChat window = new ventanaChat();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ventanaChat() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 527, 401);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
